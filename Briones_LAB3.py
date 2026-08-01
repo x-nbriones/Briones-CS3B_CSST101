0652e47675fd8b4647c1f165eb996ab713ab2714
Briones, Natalie D. <natalie.briones@lspu.edu.ph>
2026-08-01 17:32:39 +0800

    Add files via upload

---
+++ Briones_LAB3.py
@@ -0,0 +1,13 @@
+print("\n")
+name = "Natalie Briones"
+course = "BS Computer Science"
+section = "BSCS-3B"
+fav_ai_tech = "Natural Language Processing"
+future_ai_project = "An powered Earbuds that can translate languages in real-time"
+
+print("Name: ",name)
+print("Course: ",course)
+print("Section: ",section)
+print("Favorite AI Technology: ",fav_ai_tech)
+print("Future AI Project: ",future_ai_project)
+print("\n")
